b309cfb94c3b4e25bcde6b7072fee36036346b14
qrikko <qrikko@gmail.com>
2021-10-22 12:25:44 +0200

    fixed premake-file

--- premake5.lua
+++ premake5.lua
@@ -1,55 +1,13 @@
 project "simpleson"
-	location "%{localdir}"
+	location "%{dirs.localdir}"
 		
 	language "C++"
 	cppdialect "C++17"
+	kind "StaticLib"
 
-	targetdir ("%{libdir}")
+	targetdir ("%{dirs.libdir}")
 	targetname("%{prj.name}_%{cfg.buildcfg}")
-	objdir ("%{intdir}")
+	objdir ("%{dirs.intdir}")
 
-	files {
-		"**.h",
-		"**.hpp",
-		"**.inl",
-		"**.c",
-		"**.cpp",
-	}
-
-	includedirs {
-		".",
-	}
-
-	defines {"_CONSOLE"}
-
-	filter "configurations:Debug"
-		defines {"_DEBUG", "TGE_DEBUG"}
-		runtime "Debug"
-		symbols "on"
-	filter "configurations:Release"
-		defines "TGE_RELEASE"
-		runtime "Release"
-		optimize "on"
-	filter "configurations:Distribution"
-		defines "TGE_DIST"
-		runtime "Release"
-		optimize "on"
-
-	filter "system:windows"
-		kind "StaticLib"
-		staticruntime "off"
-		symbols "On"		
-		systemversion "latest"
-		--warnings "Extra"
-		--conformanceMode "On"
-		--buildoptions { "/permissive" }
-		flags { 
-		--	"FatalWarnings", -- would be both compile and lib, the original didn't set lib
-		--	"FatalCompileWarnings",
-			"MultiProcessorCompile"
-		}
-		
-		defines { 
-			"_WIN32_WINNT=0x0601",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
+	files { "json.h", "json.cpp" }
+	includedirs { "." }
